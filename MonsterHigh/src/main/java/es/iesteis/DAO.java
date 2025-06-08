225cbb11ba07ddba3551c2c1286a5b3172fa95d6
wribe <carmenga24teis@gmail.com>
2025-06-08 13:13:46 +0200

    un dia +

--- MonsterHigh/src/main/java/es/iesteis/DAO.java
+++ MonsterHigh/src/main/java/es/iesteis/DAO.java
@@ -17,7 +17,7 @@ public class DAO {
     }
 
     public HashMap<String, Integer> devolverEspecies() {
-        HashMap<String, String> animales = new HashMap<>();
+        HashMap<Integer, String> animales = new HashMap<>();
         String sql = "SELECT id, subespecie FROM subespecies where especiePrincipal like 'Animal'";
 
         try (Connection conexion = DriverManager.getConnection(url, usuario, password);
@@ -32,7 +32,7 @@ public class DAO {
         }
         System.out.println(animales);
 
-        HashMap<String, String> muertosVivientes = new HashMap<>();
+        HashMap<Integer, String> muertosVivientes = new HashMap<>();
         String sql1 = "SELECT id, subespecie FROM subespecies where especiePrincipal like 'Muerto Viviente'";
 
         try (Connection conexion = DriverManager.getConnection(url, usuario, password);
@@ -46,4 +46,24 @@ public class DAO {
             System.out.println("Error ao conectar á base de datos: " + e.getMessage());
         }
         System.out.println(muertosVivientes);
+
+        HashMap<Integer, String> otros = new HashMap<>();
+        String sql2 = "SELECT id, subespecie FROM subespecies where especiePrincipal like 'Otro'";
+
+        try (Connection conexion = DriverManager.getConnection(url, usuario, password);
+             PreparedStatement sentencia = conexion.prepareStatement(sql2);
+             ResultSet resultado = sentencia.executeQuery()) {
+
+            while (resultado.next()) {
+                otros.put(resultado.getInt("id"), resultado.getString("sub"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error ao conectar á base de datos: " + e.getMessage());
+        }
+        System.out.println(otros);
+
+        HashMap<String, HashMap<Integer, String>> especies = new HashMap<>();
+        especies.put("Animales", animales);
+        especies.put("Muertos Vivientes", muertosVivientes);
+        especies.pùt("Otros", otros);
     }
